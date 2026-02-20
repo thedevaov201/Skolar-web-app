34ba4cbce11b79112013a3db01bb54d0d08af77f
Evonex <evonex200@gmail.com>
2026-02-20 10:32:11 +0100

    feat(auth): add user logout routes and controllers

--- backend/src/modules/auth/auth.controller.js
+++ backend/src/modules/auth/auth.controller.js
@@ -45,3 +45,8 @@ export const signup = async (req, res) => {
         })
     }
 }
+
+export const logout = async (req, res) => {
+    res.clearCookie("token")
+    res.status(200).json({success: true, message: "Logged out successfully"})
+}
--- backend/src/modules/auth/auth.route.js
+++ backend/src/modules/auth/auth.route.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 
-import { signup } from './auth.controller.js';
+import { signup, logout } from './auth.controller.js';
 
 const router = Router()
 
 router.post('/signup', signup)
+router.post('/logout', logout)
 
 export default router;
