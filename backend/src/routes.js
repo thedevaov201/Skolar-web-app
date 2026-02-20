60655881bbde942da74cd404a0045570c5f09e76
Evonex <evonex200@gmail.com>
2026-02-20 09:41:59 +0100

    chore(routing): add central routes file and connect auth module
    - Created routes.js to register application routes
    - Connected auth routes under /api/auth
    - Improved routing structure for scalability

---
+++ backend/src/routes.js
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+
+import authRoutes from './modules/auth/auth.route.js';
+
+const router = Router();
+
+router.use("/auth", authRoutes)
+export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,14 @@ import express from "express"
 import dotenv from "dotenv"
 
 import { connectDB } from "./config/db.js"
+import routes from "./routes.js"
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 3000
+
+app.use("/api", routes)
 
 connectDB()
 
